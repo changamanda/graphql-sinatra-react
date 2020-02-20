50b53b26f0899bcd2702d7bd5c5e6bfa71021f95
Amanda Chang <amandachang@amandas-air.attlocal.net>
2020-02-20 16:36:54 -0600

    Add Boostrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import List from "./components/List";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 window.onload = () => {
   ReactDOM.render(<List />, document.getElementById("container"));
