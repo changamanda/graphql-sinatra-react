a25099c478494899af07a53ab06192c394b229fc
Amanda Chang <amandachang@amandas-air.attlocal.net>
2020-02-20 16:43:14 -0600

    Add data

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const List = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <div class='container'>
+      <h1>Do These Things!</h1>
+      <ul>
+        <li>Walk the dog</li>
+        <li>Buy groceries</li>
+        <li>Sweep the floor</li>
+      </ul>
+    </div>
+  );
 };
 
 export default List;
